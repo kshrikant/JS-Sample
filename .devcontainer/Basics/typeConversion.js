af62b5bf0b39adac4f4dfa293c35412d54554a7a
kshrikant <kanakattishrikant@gmail.com>
2024-05-03 08:12:29 +0000

    Sample check conversion

--- .devcontainer/Basics/typeConversion.js
+++ .devcontainer/Basics/typeConversion.js
@@ -2,10 +2,16 @@ let scrore = undefined;
 
 let valueInNumber = Number(scrore);
 
-console.log(typeof(scrore));
-console.log(typeof(valueInNumber));
-console.log(valueInNumber); //Nan - no a number
+// console.log(typeof(scrore));
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); //Nan - no a number
 
 // "1"  true "0" - false "fsdfds" - true
+
+let value  = 3
+let negValue = -value
+console.log(negValue);
+
+let trmp = "strttr"
 
 
