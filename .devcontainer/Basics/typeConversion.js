1ae168d0b6bb85de04917b2dd7f520cb861cc188
kshrikant <kanakattishrikant@gmail.com>
2024-05-03 07:42:42 +0000

    Conversion datatype

---
+++ .devcontainer/Basics/typeConversion.js
@@ -0,0 +1,11 @@
+let scrore = undefined;
+
+let valueInNumber = Number(scrore);
+
+console.log(typeof(scrore));
+console.log(typeof(valueInNumber));
+console.log(valueInNumber); //Nan - no a number
+
+// "1"  true "0" - false "fsdfds" - true
+
+
