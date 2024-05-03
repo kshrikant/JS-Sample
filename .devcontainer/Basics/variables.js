549300ac79d4ce0a0841ae7b932a3dcdc01092c1
kshrikant <kanakattishrikant@gmail.com>
2024-05-03 06:46:47 +0000

    Added variables basics

---
+++ .devcontainer/Basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 2442342
+
+let accountEmail = "test@gmail.com"
+var accountPassword = "1234"
+accountCity = "Solp"
+let accountState;
+
+// accountId = 10
+accountEmail = "sdsu@gmail.com"
+accountPassword = "323232"
+accountCity = "ioew"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
