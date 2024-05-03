0512ac8f5b4f656d508301e6ae67cb765b32f25b
kshrikant <kanakattishrikant@gmail.com>
2024-05-03 07:08:02 +0000

    Data types

---
+++ .devcontainer/Basics/dataTypes.js
@@ -0,0 +1,24 @@
+"tesst" //newer version
+
+// alert(4+6) - we are using node.js not browser
+let name = "Shrikant"
+let age = 30
+let isLoggedIn = true 
+let state = null
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined 
+
+//number 2> 53 range 
+//bigint
+//string - double or single quote
+// boolean - 
+//null - type - standalone value (empty )
+//undefined - eg let someState 
+//symbol - used for componetns uniquness in react it used most
+//object 
+//typeof - used to get type
+
+
+
+
