d3de631e39d3305fce28617e548d20441c568b68
kshrikant <kanakattishrikant@gmail.com>
2024-05-06 09:38:02 +0000

    Strings basiscs

---
+++ Basics/strings.js
@@ -0,0 +1,17 @@
+// we can user single or double quotes for strings.
+const name = "Shrikant"
+const repo = 50
+// console.log(name+repo);
+
+// console.log(`My name is ${name} and my count is ${repo}`);
+
+const newWay = new String('Test-Sample')
+// .includes('es');
+
+// console.log(newWay);
+// console.log(newWay[0]);
+// console.log(newWay.length);
+console.log(newWay.charAt(2));
+
+const test = newWay
+console.log(test.split('-'));
