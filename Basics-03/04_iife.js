ae8e6b5fcffe634bf35645def13deceda00f4782
kshrikant <kanakattishrikant@gmail.com>
2024-05-07 10:27:00 +0000

    small change

--- Basics-03/04_iife.js
+++ Basics-03/04_iife.js
@@ -1,12 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE) - to svoid global level scope pollution
 
-
+//named iife
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
 
-
+//unamed iife
 ( () => {
     console.log(`DB CONNECTED TWO`);
 } )();
