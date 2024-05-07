97c65e14e5d158549ca07b2f736b8047a8b40b6c
kshrikant <kanakattishrikant@gmail.com>
2024-05-07 06:51:53 +0000

    Array basics

---
+++ Basics-02/01_arrays.js
@@ -0,0 +1,19 @@
+const myArray = [0,1,2,3,4,"Test",false]
+console.log(myArray[6]);
+
+const array2 = new Array(1,2,3,4)
+// array2.push(6)
+
+
+// array2.shift() // will remove first 
+array2.unshift(20) // will add at 0 index
+console.log(array2);
+
+//includes to check array element in array
+//indexOf 
+//join - to combine array and generate into string
+//slice (1,3) - get from 1 to 2 index elements
+//splice (1,3 ) - get from 1 to 2 index elements *** orginal array elements get removed from splice range 
+
+
+
