ec220ec0e4f2801e45c6da92ebf5945ef34a50a3
kshrikant <kanakattishrikant@gmail.com>
2024-05-08 08:18:44 +0000

    basic condition check

---
+++ control_flow/one.js
@@ -0,0 +1,44 @@
+const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature <= 41 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+let string = "20"; 
+let number = 20; 
+  
+// console.log(string === number); //false strictly conversion
+// console.log(string == number); // true just convert value
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==3) {
+//     console.log("Allow to buy course");
+// }
+
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in");
+// }
+
+const balance = 1000
+
+if (balance > 500) console.log("test"),console.log("test2");
+
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+} else {
+    console.log("less than 1200");
+
+}
