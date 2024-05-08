8d0641b5cc7535c9789affccd5bea883690ec912
kshrikant <kanakattishrikant@gmail.com>
2024-05-08 10:17:08 +0000

    foreach with arrow

---
+++ Iterations/five.js
@@ -0,0 +1,32 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+coding.forEach(language => {
+    // console.log(language);
+});
+
+coding.forEach( (item, index, arr)=> {
+
+    // console.log(item, index, arr);
+
+})
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
+
