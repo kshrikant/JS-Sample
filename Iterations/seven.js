26cead449b535ac1e2277a7262b13e05b633aae2
kshrikant <kanakattishrikant@gmail.com>
2024-05-08 10:39:50 +0000

    map, filter and reduce

---
+++ Iterations/seven.js
@@ -0,0 +1,7 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+// console.log(newNums);
+
+const newNums = myNumers.reduce((a,b) => a+b)
+console.log(newNums);
