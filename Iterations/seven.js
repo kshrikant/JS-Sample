2dfe511841a0b327dcd045c11e6d0f07937dc467
kshrikant <kanakattishrikant@gmail.com>
2024-05-08 10:52:33 +0000

    eg reduce

--- Iterations/seven.js
+++ Iterations/seven.js
@@ -4,4 +4,27 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // console.log(newNums);
 
 const newNums = myNumers.reduce((a,b) => a+b)
-console.log(newNums);
+// console.log(newNums);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
